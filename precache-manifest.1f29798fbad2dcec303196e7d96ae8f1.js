a0e20152d293f20c6fed4d9483291eb54b18231b
unknown <mvvenkul@gmail.com>
2020-01-30 19:14:10 +0530

    Updates

---
+++ precache-manifest.1f29798fbad2dcec303196e7d96ae8f1.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c9d951d581a21adc501df0fc7bc7f3",
+    "url": "/leaderclass-homepage/index.html"
+  },
+  {
+    "revision": "f02afa693aef0ed1fed2",
+    "url": "/leaderclass-homepage/static/css/2.c2d4b67f.chunk.css"
+  },
+  {
+    "revision": "6a535d536eaaecbb5345",
+    "url": "/leaderclass-homepage/static/css/main.4ca3d8d3.chunk.css"
+  },
+  {
+    "revision": "f02afa693aef0ed1fed2",
+    "url": "/leaderclass-homepage/static/js/2.ac825653.chunk.js"
+  },
+  {
+    "revision": "21a2a98d9df7cec5f3e2510ba837dd85",
+    "url": "/leaderclass-homepage/static/js/2.ac825653.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a535d536eaaecbb5345",
+    "url": "/leaderclass-homepage/static/js/main.572e9b96.chunk.js"
+  },
+  {
+    "revision": "3bcf6d514c58b1dd0dc1",
+    "url": "/leaderclass-homepage/static/js/runtime-main.38a17511.js"
+  },
+  {
+    "revision": "eb8d98e8f02a3a5a3689c77a42f757a6",
+    "url": "/leaderclass-homepage/static/media/1.eb8d98e8.png"
+  },
+  {
+    "revision": "5579c7e5456f1ba325ecf0dadf62e369",
+    "url": "/leaderclass-homepage/static/media/10.5579c7e5.png"
+  },
+  {
+    "revision": "009aa70a8071ed63f1cf108bf77a6ff3",
+    "url": "/leaderclass-homepage/static/media/11.009aa70a.png"
+  },
+  {
+    "revision": "694c5d4fa49a4ffe97fb4e95f07de7f1",
+    "url": "/leaderclass-homepage/static/media/2.694c5d4f.png"
+  },
+  {
+    "revision": "87dee725b46b6d33b78d75ef22d24c17",
+    "url": "/leaderclass-homepage/static/media/3.87dee725.png"
+  },
+  {
+    "revision": "5745ea223d0aa8d94ee1e24b53fa6db8",
+    "url": "/leaderclass-homepage/static/media/4.5745ea22.jpg"
+  },
+  {
+    "revision": "802c5873c7f50ea1576bbf5d42d97ef3",
+    "url": "/leaderclass-homepage/static/media/5.802c5873.jfif"
+  },
+  {
+    "revision": "bce3373fa80356d0960e716b1682f267",
+    "url": "/leaderclass-homepage/static/media/6.bce3373f.jfif"
+  },
+  {
+    "revision": "afdd2e95e46ac82be3675e3cbe863eda",
+    "url": "/leaderclass-homepage/static/media/7.afdd2e95.jfif"
+  },
+  {
+    "revision": "6bf491db18d3509af21649973072aea8",
+    "url": "/leaderclass-homepage/static/media/8.6bf491db.jfif"
+  },
+  {
+    "revision": "56e2ac2f90d6778a74e3187fe2d2b44b",
+    "url": "/leaderclass-homepage/static/media/9.56e2ac2f.jfif"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/leaderclass-homepage/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/leaderclass-homepage/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/leaderclass-homepage/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "eda279176eaaa5239607f4a8eb73a09b",
+    "url": "/leaderclass-homepage/static/media/app-promotion.eda27917.svg"
+  },
+  {
+    "revision": "f4c9b519dc8e82e68b26a01332c7e0f8",
+    "url": "/leaderclass-homepage/static/media/header-bg.f4c9b519.svg"
+  },
+  {
+    "revision": "d1df2719c4934cbaa99fcf03470c8b5a",
+    "url": "/leaderclass-homepage/static/media/landing-analysis.d1df2719.svg"
+  },
+  {
+    "revision": "07fe6deb5fd6760b208a5aecd4855ca7",
+    "url": "/leaderclass-homepage/static/media/market-analysis.07fe6deb.svg"
+  },
+  {
+    "revision": "56cf92e96e0b9bdd52b23d03031a11fe",
+    "url": "/leaderclass-homepage/static/media/setup-analytics.56cf92e9.svg"
+  },
+  {
+    "revision": "987a805ec406994a26f0676f75ee4c89",
+    "url": "/leaderclass-homepage/static/media/test1.987a805e.jpg"
+  },
+  {
+    "revision": "ab188a59f5fbdfddc18d555bb8c32956",
+    "url": "/leaderclass-homepage/static/media/test2.ab188a59.jpg"
+  },
+  {
+    "revision": "e9fe38e867f2b01c38526ca8cfe6c085",
+    "url": "/leaderclass-homepage/static/media/test3.e9fe38e8.jpg"
+  },
+  {
+    "revision": "15fe24bac7ee0fd89d8f38a8f6a11c49",
+    "url": "/leaderclass-homepage/static/media/test4.15fe24ba.jpg"
+  }
+]);
